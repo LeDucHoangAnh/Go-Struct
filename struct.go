9a3636ab745640986147dce594de631e9ba65231
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 21:07:52 +0700

    khởi tạo 1 struct trong Go.

--- struct.go
+++ struct.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+//khoi tao
+type Student struct {
+	id   int
+	name string
+}
+
+func main() {
+	student1 := Student{
+		id:   123,
+		name: "Kai",
+	}
+
+	fmt.Println(student1)
 }
